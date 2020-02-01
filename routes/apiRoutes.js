15b41c93e150114ffcae3501f06d5056caa3a048
Ben Surigao <bsurigao@gmail.com>
2020-02-01 12:06:37 -0800

    adds api and pckg.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,8 @@
 var db = require("../models");
+var $ = require('jquery');
+var axios = require('axios');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
 
 module.exports = function(app) {
   // Get all examples
@@ -22,5 +26,31 @@ module.exports = function(app) {
     ) {
       res.json(dbCalCount);
     });
+  });
+
+  app.get("/api/recipes/:recipe", function(req, res) {
+    console.log('dope');
+    let userSearch = req.params.recipe;
+    let APPID = "ea609d64";
+    let APIKEY = "308c7451b81f8d1eaa36b355b451f12d";
+    let recipieURL = 'https://api.edamam.com/search?q='+userSearch+'&app_id='+APPID+'&app_key='+APIKEY+'';
+    console.log(recipieURL);
+
+    axios.get(recipieURL).then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function (response) {
+      // always executed
+      console.log("hello")
+      res.json(response);
+    });
+    
+    
+
   });
 };
