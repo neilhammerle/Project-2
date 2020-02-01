753d9af2f29107d1ec2449448d34c6ee544640d0
Neil Hammerle <neilhammerle@Neils-MacBook-Pro.local>
2020-02-01 12:27:43 -0800

    major updates

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,5 @@
 var db = require("../models");
-var $ = require('jquery');
-var axios = require('axios');
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
+var axios = require("axios");
 
 module.exports = function(app) {
   // Get all examples
@@ -29,28 +26,34 @@ module.exports = function(app) {
   });
 
   app.get("/api/recipes/:recipe", function(req, res) {
-    console.log('dope');
+    console.log("dope");
     let userSearch = req.params.recipe;
     let APPID = "ea609d64";
     let APIKEY = "308c7451b81f8d1eaa36b355b451f12d";
-    let recipieURL = 'https://api.edamam.com/search?q='+userSearch+'&app_id='+APPID+'&app_key='+APIKEY+'';
+    let recipieURL =
+      "https://api.edamam.com/search?q=" +
+      userSearch +
+      "&app_id=" +
+      APPID +
+      "&app_key=" +
+      APIKEY +
+      "";
     console.log(recipieURL);
 
-    axios.get(recipieURL).then(function (response) {
-      // handle success
-      console.log(response);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .finally(function (response) {
-      // always executed
-      console.log("hello")
-      res.json(response);
-    });
-    
-    
-
+    axios
+      .get(recipieURL)
+      .then(function(response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function(response) {
+        // always executed
+        console.log("hello");
+        res.json(response);
+      });
   });
 };
