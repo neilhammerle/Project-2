8b3422eb0e04755727b3d400e7055fe988f83530
Neil Hammerle <neilhammerle@Neils-MacBook-Pro.local>
2020-02-03 21:01:38 -0800

    updated switched from html to handlebars, added sign up and login pages

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,9 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbCalCount) {
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (dbCalCount) {
       res.render("coverpage", {
         msg: "Welcome!",
         examples: dbCalCount
@@ -12,18 +12,28 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+  app.get("/CalCount/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (
       dbCalCount
     ) {
-      res.render("example", {
+      res.render("CalCount", {
         example: dbCalCount
       });
     });
   });
 
+  // Loads sign up page in the server
+  app.get("/signup", function (req, res) {
+    res.render("signUp");
+  });
+
+  // Loads login page in the server
+  app.get("/login", function (req, res) {
+    res.render("login");
+  });
+
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
